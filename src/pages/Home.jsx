6916282fb9be55e9026ca14f0e5fa42204242267
Ashaman47 <59666448+Ashaman47@users.noreply.github.com>
2022-02-26 12:30:45 -0700

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import { ActionBanner } from ".";
 function Home() {
   return (
     <div className="home">
-     <ActionBanner
-     headerText="Apollo"
-     subText="Flashcards for interactive learning of music theory"
-     />
-    
-    </div>
+         <ActionBanner
+         headerText="Apollo"
+         subText="Flashcards for interactive learning of music theory"
+         />
+        <p>Click on the Challenges Button to begin learning</p>
+        <p>Click on the Login Button to save and report progress</p>
+        </div>
+
   );
 }
 
