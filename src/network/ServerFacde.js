db2bf54ebb9a6e0519a310bf048dfa0cbbb37262
Jaeyoung Park <jpark9509@gmail.com>
2022-03-12 15:12:03 -0700

    refiend CreateUser serverFacade

--- src/network/ServerFacde.js
+++ src/network/ServerFacde.js
@@ -2,21 +2,18 @@ import axios from 'axios';
 
 const URL = "https://67oslwoa69.execute-api.us-west-2.amazonaws.com/prod";
 
+//TODO: Test
 export async function createUser(firstName, lastName, password, is_teacher, email) {
     console.log("createUser");
-    console.log(firstName, lastName, password, is_teacher, email);
     const user_id = email;
+
     if (!user_id) {
         throw new Error("Username must not be empty");
     } else if (!password) {
         throw new Error("Password must not be empty");
     } else if (typeof is_teacher !== 'boolean') {
         throw new Error("Needs to specifiy a role (Teacher or Student)");
-    } else if (!email) {
-        throw new Error("Email must not be empty");
-    }
-
-    
+    } 
 
     try {
         var response;
@@ -27,7 +24,7 @@ export async function createUser(firstName, lastName, password, is_teacher, emai
                 firstName: firstName,
                 lastName: lastName,
             };
-            
+
             response = await axios.post(URL + '/createTeacher', body, {
                 headers: {
                     'Accept': 'application/json',
@@ -41,8 +38,7 @@ export async function createUser(firstName, lastName, password, is_teacher, emai
                 password: password,
                 firstName: firstName,
                 lastName: lastName,
-                quizzes: {},
-                teacher: undefined,
+                quizzes: {"default": 0},
             };
 
             response = await axios.post(URL + '/createUser', body, {
@@ -58,6 +54,8 @@ export async function createUser(firstName, lastName, password, is_teacher, emai
         console.log({error});
     }
 }
+
+// FIXME: password no longer needed
 export async function getUser(user, password) {
         console.log("getUser");
         console.log(user, password);
@@ -73,6 +71,66 @@ export async function getUser(user, password) {
         sendGetRequest();
 }
 
+//TODO: Test
+export async function setTeacher(user_id, teacher_id) {
+    if (!user_id) {
+        throw new Error("Username must not be empty");
+    } else if (!teacher) {
+        throw new Error("Password must not be empty");
+    } 
+
+    try {
+        var response;
+        const body =  {
+            user_id: user_id,
+            teacher: teacher_id,
+        };
+
+        response = await axios.post(URL + '/setUser', body, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            crossDomain: true,
+        });
+        console.log(response);
+    } catch(error) {
+        console.log({error});
+    }
+}
+
+//TODO: Test
+export async function setScore(user_id, quizz_id, correctNum, totalNum) {
+    if (!user_id) {
+        throw new Error("Username must not be empty");
+    } else if (!teacher) {
+        throw new Error("Password must not be empty");
+    } 
+
+    try {
+        var response;
+        const body =  {
+            user_id: user_id,
+            quizId: quizz_id,
+            correct: correctNum,
+            total: totalNum
+        };
+
+        response = await axios.post(URL + '/setUser', body, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            crossDomain: true,
+        });
+        console.log(response);
+    } catch(error) {
+        console.log({error});
+    }
+}
+
+
+//TODO: test
 export async function getTeachers() {
     const sendGetRequest = async () => {
         try {
