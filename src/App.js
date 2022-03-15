d11e3f6b7c7fb4036986a3c38c5061bd5e29f299
AmmonWarnick <warnick.ammon@gmail.com>
2022-03-15 17:38:44 -0600

    add toast for messages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
   Navigation,
   Footer,
@@ -8,7 +8,6 @@ import {
   Challenges,
   ChallengeExam,
 } from "./pages";
-import challenges from "./util/challenges.json";
 
 function App() {
   return (
--- src/components/NotesQuiz/AnswerButtons.jsx
+++ src/components/NotesQuiz/AnswerButtons.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useState } from "react";
 import { COLORS } from "../../constants";
 import styled from "styled-components";
 
