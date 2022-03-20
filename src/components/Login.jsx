b6c6eb9a9c8a990ab2cf03fb350257f7571cd106
Ricardo Leite <ricardo.leitejr95@gmail.com>
2022-03-20 14:31:22 -0600

    Polishing overall UI for Login Page

---
+++ src/components/Login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { getUser } from "../network/ServerFacde";
+
+
+const Login = ({user, password, setUserName, setPassword}) => {
+    return (
+        <div class="col-sm login-form">
+              <label for="inputEmail" class="sr-only">
+                {/* firstname + lastname (tolowercase) */}
+                User ID
+              </label>
+              <input
+                type="userName"
+                id="inputUserName"
+                class="form-control"
+                required=""
+                placeholder="Email Address"
+                autofocus=""
+                onChange={(e) => setUserName(e.target.value)}
+              />
+              <label for="inputPassword" class="sr-only">
+                Password
+              </label>
+              <input
+                type="password"
+                id="inputPassword"
+                class="form-control"
+                placeholder="Password"
+                required=""
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div class="checkbox mb-3"></div>
+              <button
+                class="btn btn-lg btn-primary btn-block"
+                onClick={() => getUser(user, password)}
+              >
+                Sign in
+              </button>{" "}
+            </div>
+    )
+}
+
+export default Login;
